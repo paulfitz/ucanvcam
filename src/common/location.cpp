cd360b31b457ef555027516e64c1dfd2ca5965fb
paul.michael.fitzpatrick <paul.michael.fitzpatrick@f8417e8f-1147-0410-bd38-07bdd6096719>
2008-04-19 03:56:21 +0000

    get command-line interface going and packaged

--- src/common/location.cpp
+++ src/common/location.cpp
@@ -6,7 +6,14 @@
 #include "registry.h"
 #include "registry_keys.h"
 
+#ifndef WIN32
+#include <unistd.h>
+#endif
+
+#include <yarp/os/Time.h>
+
 using namespace std;
+using namespace yarp::os;
 
 static string g_location = UCANVCAM_RESOURCE_PATH;
 static bool g_located = false;
@@ -23,8 +30,32 @@ std::string getResourceLocation() {
       if (result!="") {
 	//printf("Registry reports resource location %s\n", result.c_str());
 	g_location = result;
+      } else {
+#ifndef WIN32
+	if (access("media",R_OK)==0) {
+	  char buf[1000];
+	  // freefont may need complete paths
+	  getcwd(buf,sizeof(buf));
+	  g_location = buf;
+	  g_location += "/media";
+	} else if (access("../media",R_OK)==0) {
+	  char buf[1000];
+	  // freefont may need complete paths
+	  getcwd(buf,sizeof(buf));
+	  g_location = buf;
+	  g_location += "/../media";
+	} else if (access(UCANVCAM_RESOURCE_PATH,R_OK)==0) {
+	  g_location = UCANVCAM_RESOURCE_PATH;
+	} else {
+	  Time::delay(1);
+	  fprintf(stderr,"Cannot find media directory.\n");
+	  fprintf(stderr,"Continuing, but images and fonts will be missing.\n");
+	  Time::delay(2);
+	}
+#endif
       }
     }
+    g_located = true;
   }
   return g_location;
 }
