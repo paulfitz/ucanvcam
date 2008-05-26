d13642d858d912eb8823bb3a5a802f1818a257a7
paul.michael.fitzpatrick <paul.michael.fitzpatrick@f8417e8f-1147-0410-bd38-07bdd6096719>
2008-05-26 02:32:04 +0000

    picmix effect, single image

--- trunk/src/native_effects/PicmixEffect.cpp
+++ trunk/src/native_effects/PicmixEffect.cpp
@@ -15,6 +15,10 @@ using namespace yarp::sig;
 
 using namespace std;
 
+
+#include <ImageLoader.h>
+
+
 /**
  *
  * An effect STUB that uses a compiled blender still/animation as a template.
@@ -31,6 +35,7 @@ public:
     readVersion = "0";
     options.put("dir","picmix");
     options.put("version",0);
+    needRead = false;
     reconfigure(options);
   }
 
@@ -48,7 +53,7 @@ public:
     if (options.check("version")) {
       workVersion = options.check("version",Value("")).toString().c_str();
     }
-    readEffectData();
+    needRead = true;
     return true;
   }
 
@@ -64,6 +69,8 @@ private:
   string workVersion, readVersion;
   Property options;
   Property effectConfig;
+  ImageLoader img;
+  bool needRead;
 };
 
 Effect *picmixRegister() {
@@ -71,27 +78,54 @@ Effect *picmixRegister() {
 }
 
 bool PicmixEffect::draw(ImageOf<PixelRgb>& src2, ImageOf<PixelRgb>& dest2) {
-  printf("*** warning: Picmix effect not implemented yet\n");
+  if (needRead) {
+    readEffectData();
+    needRead = false;
+  }
   dest2 = src2;
+
+  if (img.isValid()) {
+    for (int x=0; x<dest2.width()&&x<img.width(); x++) {
+      for (int y=0; y<dest2.height()&&y<img.height(); y++) {
+	PixelBgra v = img.pixel(x,y);
+	if (v.a==0) {
+	  dest2(x,y) = PixelRgb(v.r,v.g,v.b);
+	} else if (v.a>=127) {
+	  // do nothing, leave copied value
+	} else {
+	  PixelRgb& o = dest2(x,y);
+	  float f = v.a/127.0;
+	  if (f>1) f = 1;
+	  int r = (int)((1-f)*v.r+f*o.r);
+	  int g = (int)((1-f)*v.g+f*o.g);
+	  int b = (int)((1-f)*v.b+f*o.b);
+	  dest2(x,y) = PixelRgb(r,g,b);
+	}
+      }
+    }
+  }
+
+
   return true;
 }
 
 void PicmixEffect::readEffectData() {
   if (workDir==readDir&&workVersion==readVersion) {
-    printf("Pixmix effect data is current for dir [%s] (version [%s])\n",
-	   readVersion.c_str(),
-	   readDir.c_str());
+    printf("Reading pixmix effect data from dir [%s] (version [%s])\n",
+	   workDir.c_str(),
+	   workVersion.c_str());
     return;
   }
 
   printf("Reading pixmix effect data from dir [%s] (version [%s])\n",
-	 readVersion.c_str(),
-	 readDir.c_str());
+	 workDir.c_str(),
+	 workVersion.c_str());
   readDir = workDir;
   readVersion = workVersion;
 
   string configFile = readDir;
   if (readDir!="") { configFile += "/"; }
+  string base = configFile;
   configFile += "config.ini";
   printf("Reading config file %s\n", configFile.c_str());
   bool ok = effectConfig.fromConfigFile(configFile.c_str());
@@ -100,4 +134,7 @@ void PicmixEffect::readEffectData() {
     return;
   }
   printf("Effect configuration is %s\n", effectConfig.toString().c_str());
+  string frame = effectConfig.check("frame",Value("default.png")).asString().c_str();
+  img.load((base+frame).c_str());
+  printf("image size %dx%d\n", img.width(), img.height());
 }
