44bf579efceb85a28b0c7a3eee4edb87007c0dc0
paul.michael.fitzpatrick <paul.michael.fitzpatrick@f8417e8f-1147-0410-bd38-07bdd6096719>
2009-06-12 05:22:32 +0000

    add basic external test program

---
+++ src/external/main.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <windows.h>
+
+#include "ShmemBus.h"
+
+/*
+
+To send an image:
+
+Modify write_test to put data in the buffer in this format:
+
+  A four byte little-endian integer holding a count that increments
+  A four byte little-endian integer holding the number 320 (width)
+  A four byte little-endian integer holding the number 240 (height)
+  A 320*240*3 byte sequence holding your image in RGBRGBRGBRGB... format
+
+*/
+
+void write_test(const char *msg) {
+  ShmemBus bus;
+  bus.init();
+  bus.beginWrite();
+  char *base = (char *)bus.buffer();
+  int len = bus.size();
+  if (strlen(msg)>=len) {
+    printf("Message too long\n");
+  } else {
+    sprintf(base,"%s",msg);
+    printf("Writing %s\n", msg);
+  }
+  bus.endWrite();
+  bus.fini();
+}
+
+void read_test() {
+  ShmemBus bus;
+  bus.init();
+  for (int i=0; i<100; i++) {
+    bus.beginRead();
+    char *base = (char *)bus.buffer();
+    printf("Read [%s]\n", base);
+    bus.endRead();
+    Sleep(1000);
+  }
+  bus.fini();
+}
+
+int main(int argc, char *argv[]) {
+  if (argc<2) {
+    read_test();
+  } else {
+    write_test(argv[1]);
+  }
+  return 0;
+}
